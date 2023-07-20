143b3aa11fef542d1c1359c20ce992e9b585d91e
crystallo <crystallo0v0@gmail.com>
2023-07-20 10:55:08 -0700

    udpated brand section to use tailwind

--- app/components/home/BrandCard.tsx
+++ app/components/home/BrandCard.tsx
@@ -1,28 +1,35 @@
-'use client';
-import styles from '@/styles/home/BrandCard.module.css';
+"use client";
+import { Variants, motion } from "framer-motion";
 
 interface BrandCardProps {
-    brand: Brand;
-    idx: number;
-    key: string;
+  brand: Brand;
+  idx: number;
+  key: string;
+}
+
+const variants = (i: number): Variants => {
+  return {};
 };
 
-export const BrandCard = ({ brand, idx, key } : BrandCardProps) => {
-    const getContainerClassName = (idx: number) => {
-        return idx % 2 ? `${styles.container} ${styles.rotate} ${styles["rotate-left"]}` :
-        `${styles.container} ${styles.rotate} ${styles["rotate-right"]}`;
-    };
+export const BrandCard = ({ brand, idx, key }: BrandCardProps) => {
+  return (
+    <motion.div
+      whileHover={{
+        scale: 1.06,
+        rotate: idx % 2 ? 3 : -3,
+      }}
+      className="relative px-4 py-4 overflow-hidden text-sm text-center align-middle bg-white shadow-lg md:py-12 rounded-xl border-1 shaodw-slate-300"
+      key={key}
+      data-test="brand-card"
+    >
+      <div className="bar absolute h-[5px] w-full top-0 left-0"></div>
+      <h1 className="font-bold font-header">{brand.name}</h1>
 
-    return (
-        <div className={`${getContainerClassName(idx)}`} key={key} data-test="brand-card">
-            <div className={`bar ${styles.bar}`}></div>
-            <h1 className={styles.title}>{brand.name}</h1>
-
-            <style jsx>{`
-                .bar {
-                    background: linear-gradient(135deg, ${brand.start}, ${brand.end});
-                }
-            `}</style>
-        </div>
-    );
-}
+      <style jsx>{`
+        .bar {
+          background: linear-gradient(135deg, ${brand.start}, ${brand.end});
+        }
+      `}</style>
+    </motion.div>
+  );
+};
--- app/components/home/Brands.tsx
+++ app/components/home/Brands.tsx
@@ -1,10 +1,12 @@
 import { brandCards } from "@/data/brandCards";
 import { BrandCard } from "./BrandCard";
-import styles from "@/styles/home/Brands.module.css";
 
 export const Brands = () => {
   return (
-    <section className={styles.container} data-test="brands-section">
+    <section
+      className="grid grid-cols-3 gap-4 mx-4 my-16 lg:gap-12 lg:grid-cols-6"
+      data-test="brands-section"
+    >
       {brandCards.map((brand, idx) => (
         <BrandCard brand={brand} idx={idx} key={brand.name} />
       ))}
